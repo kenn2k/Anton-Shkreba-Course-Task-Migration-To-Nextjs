31aca9f8483456adf9bb6c27dfce3610a3198e88
kenn2k <antonshkreba.a@gmail.com>
2026-02-14 15:57:41 +0200

    Move all project files to client

--- src/api/instances/axiosInstance.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "",
-  timeout: 4000,
-});
-
-export default axiosInstance;
--- src/components/auth/validation/loginschema.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from "yup";
-
-export const loginSchema = Yup.object().shape({
-  username: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  password: Yup.string().min(8, "Minimum 8 characters").required("Required"),
-});
